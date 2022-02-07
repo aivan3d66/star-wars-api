551d632ad450fb74222e64c0b8497f8f7d559f67
Ivan <adamovskij00@mail.ru>
2022-02-07 22:31:06 +0300

    add getHomePage api

--- src/api/api.js
+++ src/api/api.js
@@ -6,3 +6,10 @@ export const instance = axios.create({
   withCredentials: true,
   baseURL: BASE_URL,
 })
+
+
+export const homeApi = {
+  getHomePage() {
+    return instance.get(BASE_URL);
+  }
+}
