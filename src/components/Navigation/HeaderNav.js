e98d2715248794e963e74e27209e37f0c3900187
Ivan <adamovskij00@mail.ru>
2022-02-07 22:42:04 +0300

    add HeaderNav component

---
+++ src/components/Navigation/HeaderNav.js
@@ -0,0 +1,15 @@
+import {Menu} from "antd";
+import React from "react";
+
+export const HeaderNav = () => {
+  return (
+    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+      <Menu.Item key="1">People</Menu.Item>
+      <Menu.Item key="2">Planets</Menu.Item>
+      <Menu.Item key="3">Films</Menu.Item>
+      <Menu.Item key="4">Species</Menu.Item>
+      <Menu.Item key="5">Vehicles</Menu.Item>
+      <Menu.Item key="6">Starships</Menu.Item>
+    </Menu>
+  )
+}
