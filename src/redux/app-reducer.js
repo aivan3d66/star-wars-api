444a9874bc8aa93db89360e2ca65c28120892e0c
Ivan <adamovskij00@mail.ru>
2022-02-07 23:55:06 +0300

    add MAIN_PAGE action type, getMainPage AC, getMainContent thunk and logic to appReducer

--- src/redux/app-reducer.js
+++ src/redux/app-reducer.js
@@ -1,6 +1,30 @@
-export const appReducer = (state, action) => {
+import {homeApi} from "../api/api";
+
+const MAIN_PAGE = 'MAIN_PAGE';
+
+const initialState = {
+  contentMenu: ''
+}
+
+export const appReducer = (state = initialState, action) => {
   switch (action.type) {
+    case MAIN_PAGE:
+      return {
+        ...state,
+        contentMenu: action.mainData
+      }
+
     default:
       return state
   }
+}
+
+export const getMainPage = (mainData) => ({
+  type: MAIN_PAGE,
+  mainData: mainData
+})
+
+export const getMainContent = () => async (dispatch) => {
+  const response = await homeApi.getHomePage();
+  dispatch(getMainPage(response.data));
 }
