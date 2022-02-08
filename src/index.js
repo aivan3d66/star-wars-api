d4e5081b568e767947ec12de62d984674e8393eb
Ivan <adamovskij00@mail.ru>
2022-02-08 16:56:12 +0300

    add HashRouter component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
 import store from "./redux/store";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <HashRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App/>
+      </React.StrictMode>
+    </Provider>
+  </HashRouter>,
   document.getElementById('root')
 );
