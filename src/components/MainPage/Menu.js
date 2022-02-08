91ceaa9327eb5e9438ac4eb6b4e403fe6d275042
Ivan <adamovskij00@mail.ru>
2022-02-08 21:21:58 +0300

    add Menu table styles

--- src/components/MainPage/Menu.js
+++ src/components/MainPage/Menu.js
@@ -1,5 +1,4 @@
 import {useDispatch, useSelector} from "react-redux";
-import {Navigate} from 'react-router';
 import {getMainContent} from "../../redux/app-reducer";
 import {v1} from "uuid";
 import {useEffect} from "react";
@@ -14,25 +13,35 @@ export const HomeMenu = () => {
 
   const dispatch = useDispatch();
   const menu = useSelector(state => state.appPage.contentMenu);
-
   const menuList = Object.entries(menu);
-
   const menuItem = menuList.map((m, i) => {
     return (
-      <div key={v1()} className={s.tableList__item}>
-        <span className={s.tableList__itemNumber}>{i + 1}</span>
-        <span className={s.tableList__itemName}>{m[0]}:</span>
-        <span className={s.tableList__itemUrl}>{m[1]}</span>
-        <Button type="primary">
-          <NavLink to={`/${m[0]}`}>Open page</NavLink>
-        </Button>
-      </div>
+      <tr key={v1()} className={s.tableList__item}>
+        <td className={s.tableList__itemNumber}>{i + 1}</td>
+        <td className={s.tableList__itemName}>{m[0]}</td>
+        <td className={s.tableList__itemUrl}>{m[1]}</td>
+        <td>
+          <Button type="primary">
+            <NavLink to={`/${m[0]}`}>Open page</NavLink>
+          </Button>
+        </td>
+      </tr>
     )
   })
 
   return (
-    <div className={s.tableList}>
+    <table className={s.tableList}>
+      <thead>
+      <tr className={s.tableList__item}>
+        <td className={s.tableList__itemNumber}>Number</td>
+        <td className={s.tableList__itemName}>Name</td>
+        <td className={s.tableList__itemUrl}>API</td>
+        <td></td>
+      </tr>
+      </thead>
+      <tbody>
       {menuItem}
-    </div>
+      </tbody>
+    </table>
   )
 }
