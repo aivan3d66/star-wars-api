9909524c88f0d65338271da0899c51f0472f1caf
Ivan <adamovskij00@mail.ru>
2022-02-08 16:55:47 +0300

    add NavLinks and handlers

--- src/components/Navigation/HeaderNav.js
+++ src/components/Navigation/HeaderNav.js
@@ -4,13 +4,27 @@ import React from "react";
 export const HeaderNav = () => {
   return (
     <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['0']}>
-      <Menu.Item key="0">Home</Menu.Item>
-      <Menu.Item key="1">People</Menu.Item>
-      <Menu.Item key="2">Planets</Menu.Item>
-      <Menu.Item key="3">Films</Menu.Item>
-      <Menu.Item key="4">Species</Menu.Item>
-      <Menu.Item key="5">Vehicles</Menu.Item>
-      <Menu.Item key="6">Starships</Menu.Item>
+      <Menu.Item key="0">
+        <NavLink to={'/'}>Home</NavLink>
+      </Menu.Item>
+      <Menu.Item key="1" onClick={onPeopleItemHandler}>
+        <NavLink to={'/people'}>People</NavLink>
+      </Menu.Item>
+      <Menu.Item key="2" onClick={onPlanetItemHandler}>
+        <NavLink to={'/planets'}>Planets</NavLink>
+      </Menu.Item>
+      <Menu.Item key="3" onClick={onFilmsItemHandler}>
+        <NavLink to={'/films'}>Films</NavLink>
+      </Menu.Item>
+      <Menu.Item key="4" onClick={onSpeciesItemHandler}>
+        <NavLink to={'/species'}>Species</NavLink>
+      </Menu.Item>
+      <Menu.Item key="5" onClick={onVehiclesItemHandler}>
+        <NavLink to={'/vehicles'}>Vehicles</NavLink>
+      </Menu.Item>
+      <Menu.Item key="6" onClick={onStarshipsItemHandler}>
+        <NavLink to={'/starships'}>Starships</NavLink>
+      </Menu.Item>
     </Menu>
   )
 }
