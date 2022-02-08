e9de5a4b337237a1866b461ea939f8a9a956132b
Ivan <adamovskij00@mail.ru>
2022-02-08 16:54:07 +0300

    add all action types

--- src/redux/app-reducer.js
+++ src/redux/app-reducer.js
@@ -1,6 +1,12 @@
 import {homeApi} from "../api/api";
 
 const MAIN_PAGE = 'MAIN_PAGE';
+const PEOPLE_PAGE = 'PEOPLE_PAGE';
+const FILMS_PAGE = 'FILMS_PAGE';
+const SPECIES_PAGE = 'SPECIES_PAGE';
+const VEHICLES_PAGE = 'VEHICLES_PAGE';
+const STARSHIPS_PAGE = 'STARSHIPS_PAGE';
+const PLANETS_PAGE = 'PLANETS_PAGE';
 
 const initialState = {
   contentMenu: ''
