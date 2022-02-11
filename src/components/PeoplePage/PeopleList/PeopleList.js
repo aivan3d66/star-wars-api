919b108addb43d3ebf03fef4593ed4121c840cd8
Ivan <adamovskij00@mail.ru>
2022-02-11 16:20:56 +0300

    add PeopleList styles

--- src/components/PeoplePage/PeopleList/PeopleList.js
+++ src/components/PeoplePage/PeopleList/PeopleList.js
@@ -1,18 +1,19 @@
 import {useSelector} from "react-redux";
 import 'antd/dist/antd.css';
 import {Card, Spin} from "antd";
+import s from './PeopleList.module.css';
 
 export const PeopleList = () => {
   const peoples = useSelector(state => state.appPage.people.results);
 
   return (
-    <>
+    <div  className={s.peopleList__wrapper}>
       {
         !peoples
           ? <div style={{display: "flex", width: "100%"}}><Spin size={'large'} style={{margin: "0 auto"}}/></div>
           : peoples.map((p, i) => {
             return (
-              <Card title={p.name} bordered={false} style={{width: 240, margin: "0 20px 20px 0"}} key={i}>
+              <Card title={p.name} bordered={false} style={{width: 240, margin: '0 auto'}} key={i}>
                 <p>gender: {p.gender}</p>
                 <p>height: {p.height}</p>
                 <p>mass: {p.mass}</p>
@@ -20,6 +21,6 @@ export const PeopleList = () => {
             )
           })
       }
-    </>
+    </div>
   )
 }
