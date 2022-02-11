f3ab158309c3bb031aeb5999978369d9ac910e3f
Ivan <adamovskij00@mail.ru>
2022-02-11 17:45:56 +0300

    add default Filter component

--- src/components/commons/Filter/Filter.js
+++ src/components/commons/Filter/Filter.js
@@ -1,7 +1,35 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import {Select} from 'antd';
+
+const {Option} = Select;
+
+const onChange = (value) => {
+  console.log(`selected ${value}`);
+}
+
+const onSearch = (val) => {
+  console.log('search:', val);
+}
+
 export const Filter = () => {
   return (
-    <div>
-      
-    </div>
+    <Select
+      showSearch
+      placeholder="Select a filter"
+      optionFilterProp="children"
+      onChange={onChange}
+      onSearch={onSearch}
+      filterOption={(input, option) =>
+        option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+      }
+      style={{
+        width: "200px",
+        margin: "0 0 0 20px"
+      }}
+    >
+      <Option value="hot">Hot climate</Option>
+      <Option value="gravity">gravity = 1 st.</Option>
+    </Select>
   )
 }
