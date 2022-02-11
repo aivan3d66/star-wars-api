a3adb5bbe4bcdea231b1c10196761143cba8601c
Ivan <adamovskij00@mail.ru>
2022-02-11 17:26:31 +0300

    add React.memo for PlanetsPage and pagination logic

--- src/components/PlanetsPage/PlanetsPage.js
+++ src/components/PlanetsPage/PlanetsPage.js
@@ -1,13 +1,48 @@
+import React from 'react';
 import {Button} from "antd";
 import {NavLink} from "react-router-dom";
+import {Pagination} from "../commons/Pagination/Paginator";
+import {useEffect} from "react";
+import {
+  getPlanetsPageContent,
+  setCurrentPlanetsPage,
+} from "../../redux/app-reducer";
+import {useDispatch, useSelector} from "react-redux";
+import {PlanetsList} from "./PlanetsList";
 
-export const PlanetsPage = () => {
-  return (
-    <div>
-      <h2>Planets</h2>
-      <Button type="default" style={{margin: "0 0 20px 0"}}>
-        <NavLink to={`/`}>Back</NavLink>
-      </Button>
-    </div>
-  )
-}
+export const PlanetsPage = React.memo(() => {
+    const dispatch = useDispatch();
+
+    const currentPlanetPageFromState = useSelector(state => state.appPage.currentPlanetPage);
+    const totalPlanetsCount = useSelector(state => state.appPage.planets.count);
+    const pageSize = 10;
+    const portionSize = 10;
+
+    const onPageChanged = (currentPage) => {
+      dispatch(setCurrentPlanetsPage(currentPage))
+      dispatch(getPlanetsPageContent(currentPage))
+    }
+
+    useEffect(() => {
+      dispatch(getPlanetsPageContent(currentPlanetPageFromState))
+    }, [])
+
+
+    return (
+      <div>
+        <h2>Planets</h2>
+        <Button type="default" style={{margin: "0 0 20px 0"}}>
+          <NavLink to={`/`}>Back</NavLink>
+        </Button>
+        <PlanetsList/>
+        <Pagination
+          currentPage={currentPlanetPageFromState}
+          pageSize={pageSize}
+          portionSize={portionSize}
+          totalItemsCount={totalPlanetsCount}
+          onPageChanged={onPageChanged}
+        />
+      </div>
+    )
+  }
+)
