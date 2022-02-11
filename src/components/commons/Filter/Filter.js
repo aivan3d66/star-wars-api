218a6bedf796fc3a61cedf6d74531b3629a70d96
Ivan <adamovskij00@mail.ru>
2022-02-11 19:10:56 +0300

    add upd Filter component

--- src/components/commons/Filter/Filter.js
+++ src/components/commons/Filter/Filter.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import {Select} from 'antd';
+import {FILTERS, planetFilter} from "../../../redux/app-reducer";
+import {useDispatch} from "react-redux";
 
 const {Option} = Select;
 
-const onChange = (value) => {
-  console.log(`selected ${value}`);
-}
-
-const onSearch = (val) => {
-  console.log('search:', val);
-}
-
 export const Filter = () => {
+  const dispatch = useDispatch();
+  const onChange = (value) => dispatch(planetFilter(value))
+
   return (
     <Select
       showSearch
       placeholder="Select a filter"
       optionFilterProp="children"
       onChange={onChange}
-      onSearch={onSearch}
       filterOption={(input, option) =>
         option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
       }
@@ -28,8 +24,9 @@ export const Filter = () => {
         margin: "0 0 0 20px"
       }}
     >
-      <Option value="hot">Hot climate</Option>
-      <Option value="gravity">gravity = 1 st.</Option>
+      <Option value={FILTERS.CLIMATE}>Temperate climate</Option>
+      <Option value={FILTERS.GRAVITY}>Gravity = 1 st.</Option>
+      <Option value={FILTERS.ALL}>All</Option>
     </Select>
   )
 }
