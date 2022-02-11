5742421bfe435c5aad25cc5b2d42c5e84b6dc130
Ivan <adamovskij00@mail.ru>
2022-02-11 17:26:12 +0300

    add React.memo for PeoplePage

--- src/components/PeoplePage/PeoplePage.js
+++ src/components/PeoplePage/PeoplePage.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {getPeoplePageContent, setCurrentPage} from "../../redux/app-reducer";
@@ -6,38 +7,38 @@ import {NavLink} from "react-router-dom";
 import {PeopleList} from "./PeopleList/PeopleList";
 import {Pagination} from "../commons/Pagination/Paginator";
 
-export const PeoplePage = () => {
-  const dispatch = useDispatch();
+export const PeoplePage = React.memo(() => {
+    const dispatch = useDispatch();
 
-  const currentPageFromState = useSelector(state => state.appPage.currentPage);
-  const totalPeopleCount = useSelector(state => state.appPage.people.count);
-  const pageSize = 10;
-  const portionSize = 10;
+    const currentPageFromState = useSelector(state => state.appPage.currentPage);
+    const totalPeopleCount = useSelector(state => state.appPage.people.count);
+    const pageSize = 10;
+    const portionSize = 10;
 
-  const onPageChanged = (currentPage) => {
-    dispatch(getPeoplePageContent(currentPage))
-    dispatch(setCurrentPage(currentPage))
+    const onPageChanged = (currentPage) => {
+      dispatch(setCurrentPage(currentPage))
+      dispatch(getPeoplePageContent(currentPage))
+    }
+
+    useEffect(() => {
+      dispatch(getPeoplePageContent(currentPageFromState))
+    }, [])
+
+    return (
+      <div>
+        <h2>People</h2>
+        <Button type="default" style={{margin: "0 0 20px 0"}}>
+          <NavLink to={`/`}>Back</NavLink>
+        </Button>
+        <PeopleList/>
+        <Pagination
+          currentPage={currentPageFromState}
+          pageSize={pageSize}
+          portionSize={portionSize}
+          totalItemsCount={totalPeopleCount}
+          onPageChanged={onPageChanged}
+        />
+      </div>
+    )
   }
-
-  useEffect(() => {
-    dispatch(getPeoplePageContent(currentPageFromState))
-  }, [])
-
-  return (
-    <div>
-      <h2>People</h2>
-      <Button type="default" style={{margin: "0 0 20px 0"}}>
-        <NavLink to={`/`}>Back</NavLink>
-      </Button>
-      <PeopleList/>
-      <Pagination
-        currentPage={currentPageFromState}
-        pageSize={pageSize}
-        portionSize={portionSize}
-        totalItemsCount={totalPeopleCount}
-        onPageChanged={onPageChanged}
-      />
-    </div>
-
-  )
-}
+)
