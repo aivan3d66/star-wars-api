91a26606455d2d09bc8409ab395f6398bcfcf73e
Ivan <adamovskij00@mail.ru>
2022-02-11 15:19:36 +0300

    add Pagination constants

--- src/components/PeoplePage/PeoplePage.js
+++ src/components/PeoplePage/PeoplePage.js
@@ -1,16 +1,26 @@
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
-import {getPeoplePageContent} from "../../redux/app-reducer";
-import Preloader from "../commons/Preloader/SpinnerPreloader";
-import {Button, Card} from "antd";
+import {getPeoplePageContent, setCurrentPage} from "../../redux/app-reducer";
+import {Button} from "antd";
 import {NavLink} from "react-router-dom";
+import {PeopleList} from "./PeopleList";
+import {Pagination} from "../commons/Pagination/Paginator";
 
 export const PeoplePage = () => {
   const dispatch = useDispatch();
-  const peoples = useSelector(state => state.appPage.people.results);
+
+  const currentPageFromState = useSelector(state => state.appPage.currentPage);
+  const totalPeopleCount = useSelector(state => state.appPage.people.count);
+  const pageSize = 10;
+  const portionSize = 10;
+
+  const onPageChanged = (currentPage) => {
+    dispatch(getPeoplePageContent(currentPage))
+    dispatch(setCurrentPage(currentPage))
+  }
 
   useEffect(() => {
-    dispatch(getPeoplePageContent())
+    dispatch(getPeoplePageContent(currentPageFromState))
   }, [])
 
   return (
